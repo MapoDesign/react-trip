2aebdb669494a3ee66dfe34ae1e7f627255d8697
MapoDesign <elisaresmini@gmail.com>
2022-12-12 15:32:59 +0100

    footer classes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -11,13 +11,11 @@ function Footer() {
               <div className="text-container about">
                 <h4>Few Words About Me</h4>
                 <p className="white">
-                  Mi chiamo Elisa Resmini. Dopo un contratto a tempo
-                  indeterminato, finito perché l'azienda è andata in
-                  liquidazione, mi sono reinventata. Ho la passione della
-                  programmazione e lavoro su React come Front-end Developer.
-                  Avendo una formazione di design e comunicazione, ho più occhio
-                  critico dei miei colleghi e creo template davvero belli da
-                  vedere e funzionali in termini di User Experience.
+                  Mi chiamo Elisa Resmini. Ho la passione della programmazione e
+                  lavoro su React come Front-end Developer. Avendo una
+                  formazione di UX UI designer, ho più occhio critico dei miei
+                  colleghi e creo template davvero belli da vedere e funzionali
+                  in termini di User Experience.
                 </p>
               </div>
             </div>
